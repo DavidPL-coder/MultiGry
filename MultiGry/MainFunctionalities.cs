235f52fc1f2e15b418a47ba2cbf6728f9d304216
DavidPL-coder <dawid2002@hotmail.com>
2020-02-05 10:55:03 +0100

    "OptionSelector" class was edited / edytowano klasę "OptionSelector"

--- MultiGry/MainFunctionalities.cs
+++ MultiGry/MainFunctionalities.cs
@@ -22,21 +22,57 @@ namespace MultiGry
 
     class OptionSelector
     {
-        private List<IMenuOption> MenuOptions;
-
-        public void SelectingOption()
+        private enum OptionsCategory
         {
-            ConsoleKey KeySelectedByTheUser = GettingTheKeySelectingByTheUser();
-            int OptionNumber = KeySelectedByTheUser - ConsoleKey.D0;
+            Game, Wrong, ExitTheProgram
+        }
 
-            if (OptionNumber >= 1 && OptionNumber <= MenuOptions.Count)
-                MenuOptions[OptionNumber].GameExecuting();
+        private int OptionNumber;
+        private List<IMenuOption> MenuOptions;
+        
+        public void SimulationOfMainMenuOperation()
+        {
+            do
+            {
+                if (TryToSelectTheRightOption() == OptionsCategory.ExitTheProgram)
+                    return;
+            }
+            while (true);
+        }
+
+        private OptionsCategory TryToSelectTheRightOption()
+        {
+            try
+            {
+               return SelectingOption();
+            }
+            catch (InvalidOperationException exception)
+            {
+                Console.WriteLine(exception.Message);
+                return OptionsCategory.Wrong;
+            }
+        }
+
+        private OptionsCategory SelectingOption()
+        {
+            OptionNumber = GettingTheKeySelectingByTheUser() - ConsoleKey.D0;
+
+            if (IsTheNumberOfTheSelectedOptionAppropriate())
+                MenuOptions[OptionNumber - 1].GameExecuting();
+
+            else if (OptionNumber == MenuOptions.Count)
+                return OptionsCategory.ExitTheProgram;
 
             else
-                throw new InvalidOperationException("Naciśnięto niewłaściwy klawisz! (dostępne są tylko 1-9)");
+                throw new InvalidOperationException("Naciśnięto niewłaściwy klawisz! (dostępne są tylko 1-" + MenuOptions.Count + ")");
+
+            return OptionsCategory.Game;
         }
 
         private ConsoleKey GettingTheKeySelectingByTheUser() =>
             Console.ReadKey().Key;
+
+        private bool IsTheNumberOfTheSelectedOptionAppropriate() =>
+            OptionNumber >= 1 && OptionNumber <= MenuOptions.Count;
     }
 }
